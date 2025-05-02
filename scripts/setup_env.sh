6debd76e09c1f9d8b3946f257e45ada76f11c8ef
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2025-05-02 17:02:23 +0545

    solved pipeline and requirements

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [ "$CI" != "true" ]; then
+  pip install -r requirements.txt
+  python -m spacy download en_core_web_sm
+else
+  pip install -r requirements-ci.txt
+fi
+
+#!/bin/bash
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
